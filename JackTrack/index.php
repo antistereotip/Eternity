898128f019f84c8b006ceaaae65ef6d5c7a4f6b6
antistereotip <milutingavrilovic@yahoo.com>
2021-03-28 20:58:34 +0200

    JackTrackFirstTaskPart
    First part of first task, store array of data in json nodes

---
+++ JackTrack/index.php
@@ -0,0 +1,28 @@
+<?php
+//array data
+$arr = array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
+
+//output json encoded
+echo json_encode($arr);
+
+$file = json_encode($arr);
+
+//date in microseconds
+$date = round(microtime(true) * 1000);
+
+//ispis vrednosti niza
+$result = print_r($file, true);
+
+//true of nodes
+$true_of_node = 1;
+
+//all data in array
+$data = array($result, $date, $true_of_node);
+
+//spakuj sve varijable iz niza u fajl
+file_put_contents('node0.json', implode(' ',$data)."\n",FILE_APPEND | LOCK_EX);
+file_put_contents('node1.json', implode(' ',$data)."\n",FILE_APPEND | LOCK_EX);
+file_put_contents('node2.json', implode(' ',$data)."\n",FILE_APPEND | LOCK_EX);
+file_put_contents('node3.json', implode(' ',$data)."\n",FILE_APPEND | LOCK_EX);
+file_put_contents('node4.json', implode(' ',$data)."\n",FILE_APPEND | LOCK_EX);
+?>
