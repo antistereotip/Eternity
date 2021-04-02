67100e269a95fa5376efdbf8fe2ddfaa5fb91b55
antistereotip <milutingavrilovic@yahoo.com>
2021-04-02 07:08:28 +0200

    changed priority of vars

--- JackTrack/index.php
+++ JackTrack/index.php
@@ -1,24 +1,25 @@
 <?php
-//array data
-$arr = array ('id'=>1,'jfrom'=>'hightech','jto'=>'John Wick','jthrough'=>'network','jamount'=>5);
+//date in microseconds
+$date = round(microtime(true) * 1000);
 
+//true of nodes
+$true_of_node = 1;
+
+//array data
+$arr = array ('id'=>1,'jfrom'=>'hightech','jto'=>'John Wick','jthrough'=>'network','jamount'=>5, 'time'=> $date, 'true of nodes' => $true_of_node);
 
 //output json encoded
 echo json_encode($arr);
 
 $file = json_encode($arr);
 
-//date in microseconds
-$date = round(microtime(true) * 1000);
-
 //print value of array
 $result = print_r($file, true);
 
-//true of nodes
-$true_of_node = 1;
+
 
 //all data in array
-$data = array($result, $date, $true_of_node);
+$data = array($result);
 
 //put all variables in all nodes/nodes[n].json data
 //file_put_contents('nodes/node0.json', implode(' ',$data)."\n",FILE_APPEND | LOCK_EX); - testing json file node0.json
