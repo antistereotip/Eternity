ccda4e165d34bbb6009327e95424bbd38639ffe3
antistereotip <milutingavrilovic@yahoo.com>
2021-06-04 12:15:57 +0200

    meni updated

--- web/inc/meni.php
+++ web/inc/meni.php
@@ -6,23 +6,3 @@
 <hr/>
 
 
-<table>
-  <caption>Monthly savings</caption>
-  <tr>
-    <th>Pon</th>
-    <th>Month</th>
-    <th>Savings</th>
-  </tr>
-  <tr>
-    <th>Index</th>
-    <td>January</td>
-    <td>$100</td>
-  </tr>
-  <tr>
-    <th>Index</th>
-    <td>February</td>
-    <td>$50</td>
-  </tr>
-</table>
-
-
