3b23ab6177e1af4810a9486db27554985de52403
Milutin Gavrilovic <milutingavrilovic@yahoo.com>
2021-07-18 23:00:47 +0200

    Create semaphore.py
    In computer science, a semaphore is a variable or abstract data type used to control access to a common resource by multiple processes and avoid critical section problems in a concurrent system such as a multitasking operating system. A trivial semaphore is a plain variable that is changed (for example, incremented or decremented, or toggled) depending on programmer-defined conditions.

---
+++ pynode/semaphore-logic-layers.py
@@ -0,0 +1,40 @@
+#semaphore with led diods, simulate 3 layers of node processing
+from gpiozero import LED
+import time
+import keyboard
+K = input("Sleep for: ")
+zeleno = LED(18)
+zuto = LED(17)
+crveno = LED(23)
+crveno_indikator = LED(21)
+
+def dva():
+   crveno.on()
+   time.sleep(K)
+   zuto.on()
+   time.sleep(K)
+   crveno.off()
+   zuto.off()
+
+def jedan():
+   zeleno.on()
+   time.sleep(K)
+   zeleno.off()
+   zuto.on()
+   time.sleep(K)
+   zuto.off()
+   crveno.on()
+   time.sleep(K)
+
+if K == 1 :
+   while True :
+      dva()
+      jedan()
+      
+elif K == 2 :
+   while True :
+      jedan()
+
+else :
+   while True :
+      crveno_indikator.on()
